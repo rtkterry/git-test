6193edcafb823af64d23b74e8f017eac33517884
terry_wang <303917287@qq.com>
2016-11-30 14:28:58 +0800

    first commit

---
+++ makeImage_Coship_43HFA5857.sh
@@ -0,0 +1,12 @@
+Customer="COSHIP" 
+Model="43HFA5857"
+Region="CN"
+Chip="RTD2634"
+PCB="PCB-715G6682"
+Panel="LVG430DUYA1ADS2H0X"
+UI="COSHIP"
+NandFlash="MXIC_MX25L12835FM2I"
+BootLogo="JPG"
+IS2D3D="2D"
+
+./makeImage_Public_IPTV.sh $Customer $Model $Region $Chip $PCB $Panel $UI $NandFlash $BootLogo $IS2D3D
