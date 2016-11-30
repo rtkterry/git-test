356e97336be35a67106a4d0ad67867c3c1a3abbf
terry_wang <303917287@qq.com>
2016-11-30 14:37:47 +0800

    second commit

---
+++ makeImage_Coship_NJ_32PAL3257.sh
@@ -0,0 +1,12 @@
+Customer="COSHIP_NJ" 
+Model="32PAL3257"
+Region="CN"
+Chip="RTD2634"
+PCB="PCB-715G6682"
+Panel="LCA315XVN02AC7A10X"
+UI="COSHIP"
+NandFlash="WINBOND_W25Q64FVSSIG"
+BootLogo="JPG"
+IS2D3D="2D"
+
+./makeImage_Public_IPTV.sh $Customer $Model $Region $Chip $PCB $Panel $UI $NandFlash $BootLogo $IS2D3D 
